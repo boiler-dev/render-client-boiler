f13ae54cce8d51127752ac9e3a4ec768a27a0c01
winton <mail@wintoni.us>
2020-03-03 15:38:29 -0800

    Generator

---
+++ tsignore/renderClient.spec.ts
@@ -0,0 +1,8 @@
+import expect from "./expect"
+import { RenderClient } from "../src/renderClient"
+
+describe("renderClient", () => {
+  it("should instantiate", () => {
+    new RenderClient()
+  })
+})
---
+++ tsignore/renderClient.ts
@@ -0,0 +1,75 @@
+import loaded from "./loaded"
+import {
+  RenderRequest,
+  RenderResponse,
+} from "./render-server"
+
+import router from "./router"
+
+export interface App {
+  router: typeof router
+}
+
+export class RenderClient {
+  app: App = null
+  evalLoad: typeof loaded.evalLoad = null
+  libs: typeof loaded.libs = null
+
+  loaded(): void {
+    this.app.router.patchHistory(window.history.pushState)
+
+    window.onpopstate = async (): Promise<void> => {
+      const name = this.route()
+
+      const searchParams = new URLSearchParams(
+        location.search
+      )
+      const params = this.paramsToObject(
+        searchParams.entries()
+      )
+
+      await this.evalLoad({
+        [name]: window["paths"][name],
+      })
+
+      const res: RenderResponse = {}
+      const req: RenderRequest = {
+        path: window.location.pathname,
+        method: "GET",
+        files: {},
+        params,
+        user: window["user"],
+      }
+
+      const component = this.libs[name]
+      const el = await component.element(req, res)
+
+      document.body.innerHTML = ""
+
+      if (res.body) {
+        document.body.innerHTML = res.body
+      } else {
+        document.body.append(el)
+      }
+    }
+  }
+
+  paramsToObject(
+    entries: IterableIterator<[string, string]>
+  ): Record<string, string> {
+    const result = {}
+    for (const entry of entries) {
+      const [key, value] = entry
+      result[key] = value
+    }
+    return result
+  }
+
+  route(path?: string): any {
+    return this.app.router.route(
+      path || document.location.pathname
+    )
+  }
+}
+
+export default new RenderClient()
